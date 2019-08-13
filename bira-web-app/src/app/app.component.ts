57dda31b865af0844c013c0fbfaace10f6e73ddf
Ben Schoenmakers <benschoenmakers@hotmail.com>
2019-08-13 23:27:16 +0200

    dynamic-pages

--- bira-web-app/src/app/app.component.ts
+++ bira-web-app/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, OnInit, AfterContentInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NiceTextService } from './services/nice-text.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked  {
+
+  public title: string;
+
+  constructor(
+    private route: Router,
+    private textify: NiceTextService) {
+  }
+
+  ngAfterViewChecked() {
+    this.title = '';
+    const parts = this.route.url.split('/');
+    parts.reverse();
+    parts.pop();
+    parts.forEach(part => this.title += this.textify.getNiceText(part) + ' - ');
+    this.title = this.title.substring(0, this.title.lastIndexOf(' -'));
+  }
 }
