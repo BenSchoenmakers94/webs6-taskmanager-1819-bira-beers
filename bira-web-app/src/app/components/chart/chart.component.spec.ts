b53f344f1f58695625cbe28c2bb8496a25e200fe
Mark Havekes <mark.havekes@icloud.com>
2019-08-18 22:00:09 +0200

    fix unit tests

--- bira-web-app/src/app/components/chart/chart.component.spec.ts
+++ bira-web-app/src/app/components/chart/chart.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ChartComponent } from './chart.component';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/of';
+import { MatSnackBarModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FirestoreStub } from 'src/fixtures/firestore-stub';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,13 +14,24 @@ describe('ChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChartComponent ]
+      declarations: [ ChartComponent ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: AngularFirestore, useValue: FirestoreStub },
+      ],
+      imports: [ MatSnackBarModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChartComponent);
+    fixture.componentInstance.workingObject = Observable.of({
+      startDate: new Date('11-11-2019'),
+      endDate: new Date('12-12-2019'),
+      name: 'test sprint'
+    })
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- bira-web-app/src/app/components/generics/edit/edit.component.spec.ts
+++ bira-web-app/src/app/components/generics/edit/edit.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { EditComponent } from './edit.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FirestoreStub } from 'src/fixtures/firestore-stub';
+import { MatSnackBarModule } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 describe('EditComponent', () => {
   let component: EditComponent;
@@ -8,7 +14,20 @@ describe('EditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditComponent ],
+      imports: [ RouterTestingModule, MatSnackBarModule ],
+      providers: [
+        { provide: AngularFirestore, useValue: FirestoreStub },
+        { provide: ActivatedRoute, useValue: {
+            snapshot: {
+              parent: {
+                routeConfig: { path: 'test'}
+              },
+              params: { id: 1 }
+           }
+        } }
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
