76eef0c2cb2249fed10dd03eeb8b02362d3df41e
Mark Havekes <mark.havekes@icloud.com>
2019-08-21 10:55:09 +0200

    more unit tests

--- bira-web-app/src/app/services/nice-text.service.spec.ts
+++ bira-web-app/src/app/services/nice-text.service.spec.ts
@@ -9,4 +9,32 @@ describe('NiceTextService', () => {
     const service: NiceTextService = TestBed.get(NiceTextService);
     expect(service).toBeTruthy();
   });
+
+  describe('getNiceNameForId', () => {
+    it('should make id column nice', () => {
+      const service: NiceTextService = TestBed.get(NiceTextService);
+      expect(service.getNiceNameForId('userId')).toEqual('User');
+    });
+  });
+
+  describe('getTypeForId', () => {
+    it('should get the type from id column', () => {
+      const service: NiceTextService = TestBed.get(NiceTextService);
+      expect(service.getTypeForId('userId')).toEqual('users');
+    });
+  });
+
+  describe('getSingular', () => {
+    it('should get the type from id column', () => {
+      const service: NiceTextService = TestBed.get(NiceTextService);
+      expect(service.getSingular('users')).toEqual('user');
+    });
+  });
+
+  describe('getIdForType', () => {
+    it('should get the type from id column', () => {
+      const service: NiceTextService = TestBed.get(NiceTextService);
+      expect(service.getIdForType('users')).toEqual('userId');
+    });
+  });
 });
